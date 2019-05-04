6fbe99b3c8e484f8cf8d5a3720dad71ab1b7dece
Tomek Cejner <tomek@smartrecruiters.com>
2019-05-04 12:01:56 +0200

    Edycja timeboksa

--- rc/timebox_list.js
+++ rc/timebox_list.js
@@ -7,11 +7,11 @@ class PomodoroCreator extends React.Component {
     }
 
     handleSubmit = (event) => {
-        console.log('Dodajemy:',this.titleInput.current.value);
+        console.log("Dodajemy:", this.titleInput.current.value);
         event.preventDefault(); // aby się formularz nie zasubmitował
         this.props.onCreate({
             title: this.titleInput.current.value,
-            totalTimeMinutes: this.minutesInput.current.value            
+            totalTimeMinutes: this.minutesInput.current.value
         });
     };
 
@@ -20,9 +20,7 @@ class PomodoroCreator extends React.Component {
             <form onSubmit={this.handleSubmit} className="TimeboxCreator">
                 <div>
                     <label>
-                        Co robisz? <input 
-                        ref={this.titleInput} 
-                        type="text" />
+                        Co robisz? <input ref={this.titleInput} type="text" />
                     </label>
                 </div>
                 <div>
@@ -38,18 +36,123 @@ class PomodoroCreator extends React.Component {
     }
 }
 
-function Timebox({ identifier, title, totalTimeMinutes, onDelete, onEdit }) {
-    return (
-        <div className="Timebox">
-            <div className="tiny">{identifier}</div>
-            <h3>{title}</h3>
-            <p>{totalTimeMinutes} min.</p>
-            <div>
-                <button onClick={onDelete}>Usuń</button>
-                <button onClick={onEdit}>Zmień</button>
+class Timebox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false,
+            title: props.title,
+            time: props.totalTimeMinutes
+        };
+    }
+
+    onEdit = () => {
+        this.setState((prevState) => {
+            return {
+                isEditing: !prevState.isEditing
+            };
+        });
+    };
+
+    titleChanged = (event) => {
+        this.setState({
+            title: event.target.value
+        });
+    };
+
+    timeChanged = (event) => {
+        this.setState({
+            time: event.target.value
+        });
+    };
+
+    handleEdit = (params) => {
+        console.log("Edycja timeboksa, nowe wartości:", params);
+        this.props.onEdit(this.props.identifier, {
+            title: params.title,
+            totalTimeMinutes: params.minutes
+        });
+        this.setState({
+            isEditing: false
+        })
+    };
+
+    render() {
+        const { identifier, title, totalTimeMinutes, onDelete } = this.props;
+        const { isEditing } = this.state;
+        return (
+            <div className="Timebox">
+                <div className="tiny">{identifier}</div>
+                <h3>{title}</h3>
+                <p>{totalTimeMinutes} min.</p>
+                <TimeboxEditorInline
+                    visible={isEditing}
+                    title={title}
+                    minutes={totalTimeMinutes}
+                    onEdit={this.handleEdit}
+                />
+                <div>
+                    <button onClick={onDelete}>Usuń</button>
+                    <button onClick={this.onEdit}>{isEditing ? "Anuluj" : "Zmień"}</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+}
+
+class TimeboxEditorInline extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: props.title,
+            minutes: props.minutes
+        };
+    }
+
+    titleChanged = (e) => {
+        console.log("Nowy tytul: ", e.target.value);
+        this.setState({
+            title: e.target.value
+        });
+    };
+
+    timeChanged = (e) => {
+        console.log("Nowy czas: ", e.target.value);
+        this.setState({
+            minutes: e.target.value
+        });
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault(); // aby się formularz nie zasubmitował
+        this.props.onEdit({
+            ...this.state
+        });
+    };
+
+    render() {
+        const { visible } = this.props;
+        if (!visible) {
+            return null;
+        }
+        return (
+            <form className="inline-form" onSubmit={this.handleSubmit}>
+                <div className="inline-form-row">
+                    <label>
+                        Zadanie: <input type="text" value={this.state.title} onChange={this.titleChanged} />
+                    </label>
+                </div>
+                <div className="inline-form-row">
+                    <label>
+                        Czas: <input type="number" value={this.state.minutes} onChange={this.timeChanged} />
+                    </label>
+                </div>
+                <div>
+                    <button>OK</button>
+                </div>
+            </form>
+        );
+    }
 }
 
 class TimeboxList extends React.Component {
@@ -90,6 +193,15 @@ class TimeboxList extends React.Component {
         });
     };
 
+    handleEdit = (key, data) => {
+        this.setState((prevState) => {
+            const timeboxes = prevState.timeboxes.map((tb) => {
+                return tb.key === key ? { ...tb, ...data } : tb;
+            });
+            return { timeboxes };
+        });
+    };
+
     render() {
         return (
             <>
@@ -103,7 +215,8 @@ class TimeboxList extends React.Component {
                         onDelete={() => {
                             this.handleDelete(box.key);
                         }}
-                        onEdit={() => this.updateTimebox(box.key, { ...box, title: "Ulepszony" })}
+                        onEdit={this.handleEdit}
+                        isEditing={false}
                     />
                 ))}
             </>
